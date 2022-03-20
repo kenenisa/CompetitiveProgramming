9f1d3f375bbc5ca79cac5b1b9fbd0abc60d736de
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-03-20 12:32:33 +0300

    done largestNumber

---
+++ Day4/LargestNumber.js
@@ -0,0 +1,7 @@
+const nums = [3,30,34,5,9]
+
+nums.sort((x,y)=>{
+    for(let i =0;i< Math.max(`${x}`.length,`${y}`.length);i++){
+        
+    }
+})
---
+++ Day4/LargestNumber.py
@@ -0,0 +1,32 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        class CompStr(str):
+            def __lt__(x, y):
+                return x+y < y+x
+
+        largest = ''.join(map(str,sorted(nums,key=CompStr,reverse=True)))
+        return '0' if largest[0] == '0' else largest
+
+# class Solution:
+#     def largestNumber(self, nums: List[int]) -> str:
+#         def compareLargestDigit(a,b):
+#             print(a,b)
+#             comb1 = int(a + b)
+#             comb2 = int(b + a)
+#             if(comb1 > comb2):
+#                 return 1
+#             return 0
+#         for k in range(1, len(nums)):
+#             key = nums[k]
+#             i = k - 1
+
+#             while i >= 0 and compareLargestDigit(str(key), str(nums[i])):
+#                 nums[i + 1] = nums[i]
+#                 i = i - 1
+
+#             nums[i + 1] = key
+#         # print(''.join(map(str,nums)))
+#         c = ''.join(map(str,nums))
+#         return str(int(c))
+
+        
