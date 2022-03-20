9e04b284edbc1ddbcca07c24e74ab358345b2996
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-03-20 13:20:49 +0300

    done AvarageNeighbors

---
+++ Day4/AvarageNeighbors.py
@@ -0,0 +1,13 @@
+#slow, working to make it faster
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        def swapThem():
+            for i in range(1,len(nums) - 1):
+                while((nums[i-1] + nums[i+1]) / 2 == nums[i]):
+                    nums[i-1], nums[i] = nums[i], nums[i-1]
+
+            for j in range(1,len(nums) - 1):
+                if((nums[j-1] + nums[j+1]) / 2 == nums[j]):
+                    swapThem()
+        swapThem()
+        return nums
