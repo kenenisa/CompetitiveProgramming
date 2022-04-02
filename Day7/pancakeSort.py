5c15b195d9a075750b58e6c1221d8f5043f476d6
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-04-02 12:51:08 +0300

    done pancakeSort.py

---
+++ Day7/pancakeSort.py
@@ -0,0 +1,20 @@
+# finds the biggest value then takes it to it's place
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        def getMaxWithIndex(ary):
+            m = [0]
+            for i in range(len(ary)):
+                if(ary[i] > m[0]):
+                    m = [ary[i],i]
+            return m[1]
+        n = len(arr)
+        k = []
+        for i in range(n):
+            max_index = getMaxWithIndex(arr[:n]) + 1
+            if(max_index < n):
+                arr[:max_index] = reversed(arr[:max_index])
+                k.append(max_index)
+                arr[:n] = reversed(arr[:n])
+                k.append(n)
+            n -= 1
+        return k
