5a4fd32ddb632301f6070c55b70e0b01adf0bce4
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-04-10 19:28:15 +0300

    done shortestSubarray

---
+++ Day9/shortestSubarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        dp = [0] * (len(nums) + 1)
+
+        for i in range(1, len(dp)):
+            dp[i] = dp[i - 1] + nums[i - 1]
+        n = len(nums) + 1
+        Queue = deque()
+
+        for i in range(len(dp)):
+            while Queue and dp[i] - dp[Queue[0]] >= k:
+                n = min(n, i - Queue.popleft())
+            while Queue and dp[i] < dp[Queue[-1]]:
+                Queue.pop() # pop right
+            Queue.append(i)
+        return n if n != len(nums)+1 else -1
