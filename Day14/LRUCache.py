bb6b1f1356f9780a26a0f768b6c6db868d71a9d6
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-05-11 22:14:26 +0300

    done SortList

--- Day14/LRUCache.py
+++ Day14/LRUCache.py
@@ -1,3 +1,34 @@
-dic = {1:[0,20],2:[0,200],3:[0,2000]}
-for x in dic:
-    print(x)
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.access = 0
+        self.data = {}
+    def get(self, key: int) -> int:
+        if self.data.get(key):
+            self.data[key][1] = self.access
+            self.access += 1
+            return self.data[key][0]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if self.data.get(key):
+            self.data[key][0] = value 
+            self.data[key][1] = self.access
+        else:
+            if self.capacity <= 0:
+                mi = self.access
+                remove = ''
+                for x in self.data:
+                    if mi > self.data[x][1]:
+                        remove = x
+                        mi = self.data[x][1]
+                del self.data[remove]
+            self.data[key] = [value,self.access]
+            self.capacity -= 1
+        self.access += 1
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
--- Day14/MaximumTwinSumofaLinkedList.py
+++ Day14/MaximumTwinSumofaLinkedList.py
@@ -1,34 +1,21 @@
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.access = 0
-        self.data = {}
-    def get(self, key: int) -> int:
-        if self.data.get(key):
-            self.data[key][1] = self.access
-            self.access += 1
-            return self.data[key][0]
-        return -1
-
-    def put(self, key: int, value: int) -> None:
-        if self.data.get(key):
-            self.data[key][0] = value 
-            self.data[key][1] = self.access
-        else:
-            if self.capacity <= 0:
-                mi = self.access
-                remove = ''
-                for x in self.data:
-                    if mi > self.data[x][1]:
-                        remove = x
-                        mi = self.data[x][1]
-                del self.data[remove]
-            self.data[key] = [value,self.access]
-            self.capacity -= 1
-        self.access += 1
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head: Optional[ListNode]) -> int:
+        n = 0
+        temp = head
+        while temp:
+            n += 1
+            temp = temp.next
+        mx = 0
+        half = []
+        for i in range(n):
+            if i <= (n/2) -1:
+                half.append(head.val)
+            else:
+                mx = max(mx,head.val + half[n-(i+1)])
+            head = head.next
+        return mx
