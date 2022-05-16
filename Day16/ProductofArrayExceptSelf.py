0d16ab6ac32354cfb9fd40b766a1b4c1da7c3f7d
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-05-16 15:07:34 +0300

    done ProductofArrayExceptSelf

--- Day16/ProductofArrayExceptSelf.py
+++ Day16/ProductofArrayExceptSelf.py
@@ -1,3 +1,17 @@
-nums = [1,2,3,4,5]
-
-print(nums[:2] + nums[3:])
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix = [0] * n
+        suffix = [0] * n
+        prefix[0] = 1
+        suffix[-1] = 1
+        result = [0] * n
+        for i in range(1,n):
+            prefix[i] = nums[i - 1] * prefix[i - 1]
+        for j in range(n-2,-1,-1):
+            suffix[j] = nums[j + 1] * suffix[j + 1]
+        for k in range(n):
+            result[k] = prefix[k] * suffix[k]
+        return result
+        
+            
