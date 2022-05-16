dde33d99019bc9761bf3a3811c5e6796b171cf6a
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-05-16 23:36:21 +0300

    done CountNumberofNiceSubarrays

--- Day16/CountNumberofNiceSubarrays.py
+++ Day16/CountNumberofNiceSubarrays.py
@@ -4,10 +4,8 @@ class Solution:
         df[0] = 1
         prefix = 0
         result = 0
-        binary = []
         for i in nums:
-            binary.append(0 if i % 2 == 0 else 1)
-            prefix += binary[-1]
+            prefix += 0 if i % 2 == 0 else 1
             result += df[prefix - k]
             df[prefix] += 1
         return result
