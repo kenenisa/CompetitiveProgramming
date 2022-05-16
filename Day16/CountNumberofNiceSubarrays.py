a7064e228669e60c63d909ee4eefc6bd26da7616
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-05-16 23:33:02 +0300

    done CountNumberofNiceSubarrays

--- Day16/CountNumberofNiceSubarrays.py
+++ Day16/CountNumberofNiceSubarrays.py
@@ -1,16 +1,14 @@
-nums = [1,1,2,1,1]
-k = 3
-n = len(nums)
-j = 0
-mx = 0
-for i in range(n):
-    if nums[i] % 2 == 0:  continue #go to the right untill we find zeros
-    if k > 0: #exhaust zeros at first
-        k -= 1
-        continue
-    mx = max(mx,i - j)
-    while j < i and nums[j] % 2 != 0: #shrink the window from left to right
-        j += 1
-    j += 1
-mx = max(mx,n - j)
-print(mx)
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        df = defaultdict(int)
+        df[0] = 1
+        prefix = 0
+        result = 0
+        binary = []
+        for i in nums:
+            binary.append(0 if i % 2 == 0 else 1)
+            prefix += binary[-1]
+            result += df[prefix - k]
+            df[prefix] += 1
+        return result
+            
