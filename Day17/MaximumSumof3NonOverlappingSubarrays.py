30728223fb009235ee35b0d521e598c4ec4dcd73
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-05-20 08:32:44 +0300

    done MaximumSumof3NonOverlappingSubarrays

--- Day17/MaximumSumof3NonOverlappingSubarrays.py
+++ Day17/MaximumSumof3NonOverlappingSubarrays.py
@@ -1,24 +1,16 @@
 class Solution:
     def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
-        n = len(nums)
-        df = {}
-        def recur(nums, k, index, x,df):
-            if x == 3:
-                return [0, []]
-
-            if index - (x * k) > n:
-                return [0, []]
-            if (index,x) in df:
-                return df[(index,x)]
-
-            r = recur(nums, k, index + k, x + 1,df)
-            r[0] += sum(nums[index:index + k])
-
-            rn = recur(nums, k, index + 1, x,df)
-            if r[0] >= rn[0]:
-                df[(index,x)] = [r[0],([index] + r[1])]
-            else:
-                df[(index,x)] = [rn[0],rn[1]]
-            return df[(index,x)]
-        result = recur(nums, k, 0, 0,df)
-        return result[1]
+        first, second, third = sum(nums[:k]), sum(nums[k:2*k]), sum(nums[2*k:3*k])
+        sumFirst, sumSecond, sumThird = first, first+second, first+second+third
+        firstIndex, secondIndex, thirdIndex = [0], [0,k], [0,k,2*k]
+        for i in range(1, len(nums)-3*k+1):
+            first += nums[i-1+k] - nums[i-1]
+            second += nums[i-1+2*k] - nums[i-1+k]
+            third += nums[i-1+3*k] - nums[i-1+2*k]
+            if first > sumFirst:
+                sumFirst, firstIndex = first, [i]
+            if sumFirst + second > sumSecond:
+                sumSecond, secondIndex = sumFirst + second, firstIndex + [i+k]
+            if sumSecond + third > sumThird:
+                sumThird, thirdIndex = sumSecond + third, secondIndex + [i+2*k]
+        return thirdIndex
