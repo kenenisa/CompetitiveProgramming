76ee1c425491992ef7f52e4f119bad2b8b555c59
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2022-09-01 17:34:00 +0300

    Create Dota2Senate.py

---
+++ Day24/Dota2Senate.py
@@ -0,0 +1,21 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        r = deque()
+        d = deque()
+        
+        for i,x in enumerate(senate):
+            if x == "R":
+                r.append(i)
+            else:
+                d.append(i)
+                
+        while r and d:
+            if r[0] < d[0]:
+                r.append(r.popleft()+n)
+                d.popleft()
+                continue
+            r.popleft()
+            d.append(d.popleft() + n)
+        
+        return "Radiant" if r else "Dire"
