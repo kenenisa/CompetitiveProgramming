771477a9c54dec77b4665f3bd2a5dbd6a6b22eb2
kenenisa <kenenisaalemayhu0@gmail.com>
2022-12-23 21:26:22 +0300

    ll

--- Day30/valid-palindrome.py
+++ Day30/valid-palindrome.py
@@ -1,4 +1,14 @@
+# with two pointers
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = re.sub('[^0-9a-zA-Z]+', '', s).lower()
-        return s == s[::-1]
+        n = len(s)
+        for i in range(n//2):
+            if s[i] != s[n - i - 1]:
+                return False
+        return True
+# fastest
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = re.sub('[^0-9a-zA-Z]+', '', s).lower()
+#         return s == s[::-1]
