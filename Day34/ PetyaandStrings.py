a4c1e8f144644d4ec6ece20b3c5d50d0e74b8d13
kenenisa <kenenisaalemayhu0@gmail.com>
2022-12-30 21:15:06 +0300

    iv

---
+++ Day34/ PetyaandStrings.py
@@ -0,0 +1,17 @@
+a = input().lower()
+b = input().lower()
+if a == b:
+    print(0)
+else:
+    alphabet = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
+    v = True
+    l = len(a)
+    for i in range(l):
+        w1 = alphabet[a[i]]
+        w2 = alphabet[b[i]]
+        if w1 < w2:
+            break
+        elif w1 > w2:
+            v = False
+            break
+    print(-1 if v else 1)
