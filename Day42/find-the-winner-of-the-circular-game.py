5116f767a87d8d558f08783170284464948846f6
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2023-01-18 00:40:39 +0300

    Create find-the-winner-of-the-circular-game.py

---
+++ Day42/find-the-winner-of-the-circular-game.py
@@ -0,0 +1,22 @@
+# class Solution:
+#     def findTheWinner(self, n: int, k: int) -> int:
+#         players = list(range(1,n+1))
+#         def play(ary,n,l):
+#             if len(ary) == 1:  return ary[0]
+#             turn = (l % n) - 1
+#             ary.pop(turn)
+#             l = turn + k if turn != -1 else k
+#             n -= 1
+#             return play(ary,n,l)
+#         return play(players,n,k)
+
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        players = list(range(1,n+1))
+        l = k
+        while n != 1:
+            turn = (l % n) - 1
+            players.pop(turn)
+            l = turn + k if turn != -1 else k
+            n -= 1
+        return players[0]
