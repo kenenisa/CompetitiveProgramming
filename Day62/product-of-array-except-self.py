83d665c4590c3f1d1ce63b7b11260dc00190f97f
kenenisa <kenenisaalemayhu0@gmail.com>
2023-03-09 19:46:02 +0300

    2

---
+++ Day62/product-of-array-except-self.py
@@ -0,0 +1,24 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        zeroes = 0
+        acc = 1
+
+        for i in range(n):
+            if nums[i] == 0:
+                zeroes += 1
+            else:
+                acc *= nums[i]
+        
+        result = [0]*n
+        if zeroes > 1:
+            return [0]*n
+        elif zeroes == 1:
+            for i in range(n):
+                if nums[i] == 0:
+                    result[i] = acc
+                    return result
+        else:
+            for i in range(n):
+                result[i] = acc//nums[i]
+            return result
