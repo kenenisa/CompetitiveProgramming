1876f068e09a96960d596cc1d268f45283b8de36
Kenenisa Alemayehu <kenenisaalemayhu0@gmail.com>
2023-05-06 22:59:12 +0300

    4

--- Day81/single-number-ii.py
+++ Day81/single-number-ii.py
@@ -1,10 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        once = set()
-        twice = set()
-        for num in nums:
-            if num in once:
-                twice.add(num)
-            else:
-                once.add(num)
-        return once.difference(twice).pop()
+        base1 = 0
+        base2 = 0
+        for n in nums:
+            base1 ^=  n & ~base2
+            base2 ^=  n & ~base1
+        return base1
