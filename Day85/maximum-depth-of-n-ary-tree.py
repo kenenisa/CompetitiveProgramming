9a078b6312015708bafde1cc521151c9ef63cc7b
Kenenisa Alemayehu <kenenisaalemayhu0@gmail.com>
2023-05-17 21:22:51 +0300

    7

---
+++ Day85/maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        global deepest
+        deepest = 0
+
+        def dfs(node,depth):
+            global deepest
+            if node:
+                depth += 1
+
+                deepest = max(deepest,depth)
+
+                for n in node.children:
+                    dfs(n,depth)
+        dfs(root,0)
+        return deepest
