9bf200762284aa60309c767632347d01e50fa70a
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2023-11-11 00:22:40 +0300

    Update get-maximum-in-generated-array.py

--- Day101/get-maximum-in-generated-array.py
+++ Day101/get-maximum-in-generated-array.py
@@ -1,1 +1,14 @@
-        if n < 2: return nums[n]
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        if n == 0: return 0
+        if n == 1: return 1
+        nums = [0]*(n+1)
+        nums[0] = 0
+        nums[1] = 1
+        for i in range(1,100):
+            if 2 <= 2 * i <= n:
+                nums[2*i] = nums[i]
+            
+            if 2 <= 2 * i + 1 <= n:
+                nums[2*i+1] = nums[i] + nums[i+1]
+        return max(nums)
